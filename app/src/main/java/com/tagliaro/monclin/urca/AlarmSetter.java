6a868c847e2fc632ec0ee9cacdbb97d9eacf94a5
BastienTagliaro <bastien.tagliaro@gmail.com>
2018-03-24 18:46:08 +0100

    Logging information and getting preferences

--- app/src/main/java/com/tagliaro/monclin/urca/AlarmSetter.java
+++ app/src/main/java/com/tagliaro/monclin/urca/AlarmSetter.java
@@ -7,15 +7,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.util.Log;
 
 import java.util.Calendar;
 
 public class AlarmSetter extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        System.out.println("Received intent");
+        Log.d("AlarmSetter", "Received intent");
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-        Integer intervalInMinutes = sharedPreferences.getInt("interval", 30);
+        Integer intervalInMinutes = Integer.parseInt(sharedPreferences.getString("sync_frequency", "30"));
 
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
 
