549a1eba33ba0bbf3000936df1f3c223087693c2
BastienTagliaro <bastien.tagliaro@gmail.com>
2018-03-24 19:00:20 +0100

    Various changes. Added permission check, put code into different functions. Added the settings menu.

--- app/src/main/java/com/tagliaro/monclin/urca/MainActivity.java
+++ app/src/main/java/com/tagliaro/monclin/urca/MainActivity.java
@@ -1,15 +1,27 @@
 package com.tagliaro.monclin.urca;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
-import android.preference.PreferenceManager;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.CalendarView;
@@ -23,68 +35,138 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
     private BroadcastReceiver updateReceiver;
     private String currentDate;
+    CalendarView calendarView;
+    ListView classesList;
+    private final int STORAGE_PERMISSION_CODE = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final CalendarView calendarView = findViewById(R.id.calendarView);
-        final ListView classesList = findViewById(R.id.listView);
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy", Locale.FRANCE);
-        currentDate = dateFormat.format(calendarView.getDate());
+        calendarView = findViewById(R.id.calendarView);
+        classesList = findViewById(R.id.listView);
 
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions();
+        }
 
-        // Temporary, this will be up to the user to choose in settings
-        editor.putString("agenda", "http://agenda.univ-reims.fr/ical.php?cle=33fc8e511b003f20a2a9cbbb3c5eec2e");
-        editor.putInt("interval", 2);
-        editor.apply();
+        else {
+            process();
+        }
+    }
 
-        File folder = new File(Environment.getExternalStorageDirectory() + File.separator + getPackageName());
+    private Boolean createFolder(String name) {
+        File folder = new File(Environment.getExternalStorageDirectory() + File.separator + name);
         Boolean success = true;
 
         if(!folder.exists())
             success = folder.mkdirs();
 
-        setListItems(this, R.layout.classes_view, currentDate);
+        return success;
+    }
 
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int day) {
-                month += 1;
-                currentDate = String.format(Locale.FRANCE, "%02d", day) + "-" + String.format(Locale.FRANCE, "%02d", month) + "-" + Integer.toString(year);
-                setListItems(getApplicationContext(), R.layout.classes_view, currentDate);
+    private void process() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy", Locale.FRANCE);
+        currentDate = dateFormat.format(calendarView.getDate());
+
+        if(createFolder(getPackageName())) {
+            setListItems(this, R.layout.classes_view, currentDate);
+
+            calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+                @Override
+                public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int day) {
+                    month += 1;
+                    currentDate = String.format(Locale.FRANCE, "%02d", day) + "-" + String.format(Locale.FRANCE, "%02d", month) + "-" + Integer.toString(year);
+                    setListItems(getApplicationContext(), R.layout.classes_view, currentDate);
+                }
+            });
+
+            classesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                    Long classID = (Long) view.getTag();
+
+                    Intent intent = new Intent(getApplicationContext(), DetailsActivity.class);
+                    intent.putExtra("id", classID);
+                    startActivity(intent);
+                }
+            });
+
+            updateReceiver = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    Log.d("MainActivity","Received update broadcast, refreshing");
+
+                    setListItems(context, R.layout.classes_view, currentDate);
+                }
+            };
+
+            // This is just to make sure the AlarmSetter is always running the Alarm
+            Intent intent = new Intent("com.tagliaro.monclin.urca.RESET_ALARM");
+            intent.setClass(this, AlarmSetter.class);
+            sendBroadcast(intent);
+
+            registerReceiver(updateReceiver, new IntentFilter("urca.UPDATE_CALENDAR"));
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode == STORAGE_PERMISSION_CODE) {
+            for(int i = 0; i < permissions.length; ++i) {
+                String permission = permissions[i];
+
+                if(grantResults[i] == PackageManager.PERMISSION_DENIED) {
+                    boolean showRationale = shouldShowRequestPermissionRationale(permission);
+
+                    if(!showRationale) {
+                        Log.d("MainActivity", "Test");
+                        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                        Uri uri = Uri.fromParts("package", getPackageName(), null);
+                        intent.setData(uri);
+                        startActivityForResult(intent, STORAGE_PERMISSION_CODE);
+                    }
+                    else if (Manifest.permission.WRITE_EXTERNAL_STORAGE.equals(permission)) {
+                        showRationale(R.string.permission_storage_required);
+                    }
+                }
+                else {
+                    process();
+                }
             }
-        });
+        }
 
-        classesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                Long classID = (Long) view.getTag();
+        Log.d("MainActivity", "Testttt");
+    }
 
-                Intent intent = new Intent(getApplicationContext(), Details.class);
-                intent.putExtra("id", classID);
-                startActivity(intent);
-            }
-        });
+    @TargetApi(Build.VERSION_CODES.M)
+    private void showRationale(int resource) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(resource).setTitle("Permission denied");
 
-        updateReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                System.out.println("Received broadcast, refreshing");
+        builder.setPositiveButton("RE-TRY", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        requestPermissions();
+                    }
+                });
 
-                setListItems(context, R.layout.classes_view, currentDate);
-            }
-        };
+        builder.setNegativeButton("I'M SURE", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        finishAndRemoveTask();
+                    }
+                });
 
-        // This is just to make sure the AlarmSetter is always running the Alarm
-        Intent intent = new Intent("com.tagliaro.monclin.urca.RESET_ALARM");
-        intent.setClass(this, AlarmSetter.class);
-        sendBroadcast(intent);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
 
-        registerReceiver(updateReceiver, new IntentFilter("urca.UPDATE_CALENDAR"));
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.WRITE_EXTERNAL_STORAGE }, STORAGE_PERMISSION_CODE);
     }
 
     private void setListItems(Context context, int resource, String date) {
@@ -92,8 +174,28 @@ public class MainActivity extends AppCompatActivity {
         ListView classesList = findViewById(R.id.listView);
 
         List<Cours> cours = databaseHandler.getCours(date);
-        ListAdapter customAdapter = new ListAdapter(getApplicationContext(), resource, cours);
+        ClassesListAdapter customAdapter = new ClassesListAdapter(getApplicationContext(), resource, cours);
         classesList.setAdapter(customAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_pref, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.menu_pref:
+                Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
